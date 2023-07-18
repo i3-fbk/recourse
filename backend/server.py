4d6ffaafada2ae094b9e24afccfb00a7ad81f518
Giovanni De Toni <giovanni.det@gmail.com>
2023-07-18 09:50:58 +0200

    Refactor of the backend.

--- backend/server.py
+++ backend/server.py
@@ -83,23 +83,6 @@ environment_configs = {
     k: v.environment_config for k,v in recourse_method.items()
 }
 
-# /ask
-# The route should accept a JSON object which comprises:
-# - User features;
-# - User costraints;
-# We would save as cookie the user weights, such to retrieve them
-# each time they enter the system.
-#
-# It would then return a JSON object representing the proposed
-# recourse plan, which would be rendered by the app.
-
-@app.route("/")
-def hello_world():
-    x = dataset.sample()
-    result = ensemble.predict(x)
-
-    return f"<p>{str(x)}</p><br /><p>{result}</p>"
-
 @app.route("/get_user", methods=['GET'])
 @cross_origin(supports_credentials=True)
 def get_user():
@@ -111,101 +94,31 @@ def get_user():
 @cross_origin(supports_credentials=True)
 def get_recourse():
 
-    # We extract the user preferences, data and weigths from the cookies
-    # If those values are none, we will set a default value
-    user_data_and_preferences = request.get_json(force=True, silent=True)
-
-    # TODO: VERY DANGEROUS!!!! We need a way to sanitize the cookie to prevent code execution.
-    # Once everything is dockerized, it should not be an issue (they would destroy only the docker).
-    # We are restricting the usage to only __builtins__ but it could still be dangerous.
-    user_current_weights = None
-    if request.cookies.get("RecourseInteractiveWeights23", None):
-        user_current_weights = eval(request.cookies.get("RecourseInteractiveWeights23", None), {})
-
-    if user_data_and_preferences is None:
-        user_data = dataset.sample()
-        user_preferences = {}
-    else:
-        # Convert the user and get its preferences
-        user_preferences = user_data_and_preferences.get("preferences", {})
-        user_data = {}
-        for dataset_type in user_data_and_preferences.get("features"):
-            current_features = user_data_and_preferences.get("features").get(dataset_type)
-            user_data[dataset_type] = pd.DataFrame.from_records([{v.get("name"):v.get("value") for v in current_features}])
-
-    if user_current_weights is None:
-        user_current_weights = {k: np.random.randint(1,100, size=len(user_data.get(k).columns)) for k,v in user_data.items()}
-    
-    user_current_weights = {k: pd.DataFrame([user_current_weights.get(k)], columns=user_data.get(k).columns) for k,v in user_data.items()}
+    # Get user data, preferences, current_weights and plans
+    # In this case, we discard the previous plans
+    user_data, user_preferences, user_current_weights, _ = extract_features_and_preferences()
 
     # Potential interventions
-    potential_interventions = dict()
-
-    for k, v in user_data.items():
-
-        temp_potential_interventions = []
-        previous_solutions = []
-        current_traces = []
-
-        for _ in range(0,3):
-            
-            df_cfs, Y_full, competitor_traces, costs_efare, root_node = recourse_method.get(k).predict(v,
-                                                                                                    user_current_weights.get(k),
-                                                                                                    full_output=True,
-                                                                                                    verbose=False,
-                                                                                                    mcts_steps=5,
-                                                                                                    noise=0.3,
-                                                                                                    previous_solutions=previous_solutions,
-                                                                                                    user_constraints=user_preferences)
-            if Y_full[0] == 1:
-                
-                trace_tuple = "---".join([f"{p}_{a}" for p,a in competitor_traces[0]])
-                
-                if trace_tuple not in current_traces:
-                    
-                    temp_potential_interventions.append((competitor_traces[0], costs_efare[0], df_cfs.to_dict('records')[0]))
-                    
-                    current_traces.append(trace_tuple)
-                    previous_solutions.append(
-                        df_cfs.to_dict('records')[0]
-                    )
-
-        # Pick only the MAX_RECOURSE_PLANS plans with the lowest cost
-        temp_potential_interventions = sorted(temp_potential_interventions, key=lambda x: x[1])
-        temp_full_plans = [x[0] for x in temp_potential_interventions[0:MAX_RECOURSE_PLANS]]
-        temp_potential_interventions = [x[2] for x in temp_potential_interventions[0:MAX_RECOURSE_PLANS]]
-
-        # Save the top interventions there
-        potential_interventions[k] = (temp_potential_interventions, temp_full_plans)
+    potential_interventions = generate_interventions(user_data, user_current_weights, user_preferences)
 
     return convert_plans_into_json(potential_interventions, {k:v.to_dict('records')[0] for k,v in user_data.items()}, user_preferences)
 
-@app.route("/ask", methods=['POST', 'GET'])
-def return_recourse():
-    error = None
-    if request.method == 'POST':
-        pass
-    elif request.method == 'GET':
+@app.route("/get_recourse_v2", methods=['GET', 'POST'])
+@cross_origin(supports_credentials=True)
+def get_recourse_and_learn():
 
-        x = dataset.sample()
-        result = ensemble.predict(x)
+    # Get user data, preferences, current_weights and plans
+    user_data, user_preferences, user_current_weights, recourse_previous_plans = extract_features_and_preferences()
 
-        return f"<p>{str(x)}</p><br /><p>{result}</p>"
+    # Compute the new weights
+    new_weights = improve_weights(user_data, user_current_weights, recourse_previous_plans)
+    new_weights = {k: pd.DataFrame([new_weights.get(k)], columns=user_data.get(k).columns) for k,v in user_data.items()}
 
-    return f"{error}"
+    # Potential interventions
+    potential_interventions = generate_interventions(user_data, new_weights, user_preferences)
 
+    return convert_plans_into_json(potential_interventions, {k:v.to_dict('records')[0] for k,v in user_data.items()}, user_preferences)
 
-"""
-previous_plans = [
-
-    {   
-        overall_satisfaction: 1
-        plan: [(action, value), (action_value)]
-    }
-
-]
-
-"""
 
 def extract_features_and_preferences():
 
@@ -251,18 +164,21 @@ def extract_features_and_preferences():
 
     return user_data, user_preferences, user_current_weights, recourse_previous_plans
 
-@app.route("/get_recourse_v2", methods=['GET', 'POST'])
-@cross_origin(supports_credentials=True)
-def get_recourse_and_learn():
 
-    # Get user data, preferences, current_weights and plans
-    user_data, user_preferences, user_current_weights, recourse_previous_plans = extract_features_and_preferences()
+def generate_interventions(user_data: dict, user_weights: dict, user_preferences: dict) -> dict:
+    """Generate recourse plans.
 
-    # Compute the new weights
-    new_weights = improve_weights(user_data, user_current_weights, recourse_previous_plans)
-    new_weights = {k: pd.DataFrame([new_weights.get(k)], columns=user_data.get(k).columns) for k,v in user_data.items()}
+    :param user_data: user features 
+    :type user_data: dict
+    :param user_weights: user weights
+    :type user_weights: dict
+    :param user_preferences: user preferences
+    :type user_preferences: dict
+    :return: the recourse plans
+    :rtype: dict
+    """
 
-    # Potential interventions
+
     potential_interventions = dict()
 
     for k, v in user_data.items():
@@ -276,7 +192,7 @@ def get_recourse_and_learn():
             print(f"generating id {int_idx}, {k}")
             
             df_cfs, Y_full, competitor_traces, costs_efare, root_node = recourse_method.get(k).predict(v,
-                                                                                                    new_weights.get(k),
+                                                                                                    user_weights.get(k),
                                                                                                     full_output=True,
                                                                                                     verbose=False,
                                                                                                     mcts_steps=5,
@@ -304,10 +220,23 @@ def get_recourse_and_learn():
         # Save the top interventions there
         potential_interventions[k] = (temp_potential_interventions, temp_full_plans)
 
-    return convert_plans_into_json(potential_interventions, {k:v.to_dict('records')[0] for k,v in user_data.items()}, user_preferences)
+    return potential_interventions
 
+def merge_user_preferences(previous_preferences: dict, new_preferences: dict) -> dict:
+    """Merge the current user preferences with the new ones.
 
-def merge_user_preferences(previous_preferences: dict, new_preferences: dict):
+    The strategy is the following: for continuous values, we take the minimum between the
+    current and new maximum value, and we take the maximum between the current and new
+    minimum value. For categorical values, we simply replace the current selection with
+    the new one. Lastly, we add the new preferences if they were not specified before. 
+
+    :param previous_preferences: previous preferences as dictionary {feature_name: preferences}
+    :type previous_preferences: dict
+    :param new_preferences: new preferences as dictionary
+    :type new_preferences: dict
+    :return: updated preferences
+    :rtype: dict
+    """
 
     updated_preferences = previous_preferences.copy()
 
@@ -339,7 +268,18 @@ def merge_user_preferences(previous_preferences: dict, new_preferences: dict):
 
     return updated_preferences
 
-def improve_weights(X: dict, previous_w: dict, previous_plans: dict):
+def improve_weights(X: dict, previous_w: dict, previous_plans: dict) -> dict:
+    """Update the current user weights given new interaction
+
+    :param X: user features
+    :type X: dict
+    :param previous_w: previous weights
+    :type previous_w: dict
+    :param previous_plans: previous plans ranked
+    :type previous_plans: dict
+    :return: new user weights
+    :rtype: dict
+    """
     
     previous_plans = sorted(previous_plans, key=lambda x: x.get("overall_satisfaction"), reverse=True)
     constraints = {}
