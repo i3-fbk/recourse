6dbdb1cc5a1fc18895476a9a30f079c0b542a9ea
delaramesfahani <delaramesfahani73@gmail.com>
2023-08-14 23:25:49 +0200

    mongodb

--- src/logger.js
+++ src/logger.js
@@ -7,6 +7,7 @@ const logs = [];
 export function logEvent(userId, event, parameter) {
   const timestamp = new Date().toISOString();
 
+  const defaultLink = 'http://127.0.0.1:5000'
   const log = {
       "timestamp" : timestamp,
       "user ID": userId,
@@ -15,10 +16,10 @@ export function logEvent(userId, event, parameter) {
   }
 
   logs.push(log);
-  console.log('logs',logs)
+ 
 
   axios
-  .post("http://127.0.0.1:5000/insert_logs", {log})
+  .post(`${defaultLink}/insert_logs`, log)
   .then((res) => {
       console.log(`Server responded with status code ${res.status}`);
   })
